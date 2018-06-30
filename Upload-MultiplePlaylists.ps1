927f8c46b55df3f836ea2fd1062475607d8ade0d
vmkernel <Aleksey.I.Ivanov@gmail.com>
2018-06-30 23:59:57 +0300

    multiple playlists processing - initial commit

---
+++ Upload-MultiplePlaylists.ps1
@@ -0,0 +1,5 @@
+# This script loads a list of playlists, destination folders and AllInOne flags from the specified CSV file and invokes Upload-Playlist cmdlet for them
+# The mail puprose is to avoid manual command typing for each playlist
+# © Aleksey Ivanov, 2018
+
+# TODO: Implement logic
